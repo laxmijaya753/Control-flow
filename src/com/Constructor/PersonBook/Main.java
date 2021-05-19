f020419b61f4061b3c87550591b86a8700b5c7ff
beera@DESKTOP <you@example.com>
2021-05-19 15:48:05 -0400

    Jaya:Adding new java files

---
+++ src/com/Constructor/PersonBook/Main.java
@@ -0,0 +1,14 @@
+package com.Constructor.PersonBook;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person p=new Person("Jaya",35,"Female");
+		Book b= new Book("The Funny Stories",100,"3Edition");
+		p.read(b);
+		
+
+	}
+
+}
---
+++ src/com/Numbers/Task1/Numbertask1.java
@@ -0,0 +1,14 @@
+package com.Numbers.Task1;
+
+public class Numbertask1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(print(10));
+
+	}
+public static int print(int x)
+{
+	return x;
+}
+}
